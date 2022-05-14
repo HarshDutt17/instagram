a869f72962666d20a3d6c826014903d1b85f06c9
HarshDutt17 <harshdatt17@gmail.com>
2022-05-14 13:38:50 +0530

    seeded firebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
-  </React.StrictMode>
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// client side rendered app : react (cra)
+// -> database which is Firebase
+// -> react loading skeleton
+// tailwind
+
+// folder structure
+// src
+// -> components
+// -> constants
+// -> context
+// -> helpers
+// -> hooks
+// -> pages
+// -> lib (firebase is going to live in here)
+// -> services (firebase functions in here)
+// -> styles (tailwind's folder (app/tailwind))
---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+// import seed file
+import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyAqNpMEaCynj3_FxAr5nCrvnQFDUXarYls',
+  authDomain: 'instagram-hd-a1da8.firebaseapp.com',
+  projectId: 'instagram-hd-a1da8',
+  storageBucket: 'instagram-hd-a1da8.appspot.com',
+  messagingSenderId: '886976400704',
+  appId: '1:886976400704:web:c8b5faa328091c8cb0bea5'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// call seed file once
+seedDatabase(firebase);
+
+export { firebase, FieldValue };
