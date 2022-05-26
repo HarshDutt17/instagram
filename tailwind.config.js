f38f2bf37e21d5e89b23d15ecf6aadb8665fe16d
HarshDutt17 <harshdatt17@gmail.com>
2022-05-26 12:39:21 +0530

    unused styles purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true
   },
-
+  purge: {
+    content: ['./src/**/*.js', './src/**/**/*.js']
+  },
 
   theme: {
     fill: (theme) => ({
