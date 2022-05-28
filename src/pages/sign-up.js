66cca41a4ee1efa7517c8833517c7d79e08bb4ea
HarshDutt17 <harshdatt17@gmail.com>
2022-05-28 13:12:49 +0530

    displayname-lowercase

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -37,7 +37,7 @@ export default function SignUp(){
                 // console.log(createdUserResult, createdUserResult.user)
                     
                 await createdUserResult.user.updateProfile({
-                    displayName: username
+                    displayName: username.toLowerCase(),
                 });
 
                 // firebase user collection (create a document)
